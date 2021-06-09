b632f08790ee5f9e116581f6111782134450fd92
Brandon Parker <brandon.ch.parker@gmail.com>
2021-06-08 21:54:31 -0700

    Created basic framework for ratings and reviews

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductOverview from '../modules/ProductOverview/ProductOverview.jsx';
+import RatingsAndReviews from '../modules/ratingsAndReviews/ratingsAndReviews.jsx';
 
 
 const App = function () {
   return (
     <div>
       <ProductOverview />
+      <RatingsAndReviews/>
     </div>
   );
 }
