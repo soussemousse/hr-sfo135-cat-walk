bc74f49c2fc109f88294d671eb6e9879cb671106
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-06-08 19:23:19 -0700

    Added dynamically rendering Related Products list and cards with sample data.

--- client/src/modules/RelatedProducts/RelatedCard.jsx
+++ client/src/modules/RelatedProducts/RelatedCard.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+function RelatedCard (props) {
+  return (
+    <article className='card'>
+      <img></img>
+      <small>{props.product.category}</small>
+      <h4>{props.product.name}</h4>
+      <span>{props.product.default_price}</span><br></br>
+      <span>Rating</span>
+    </article>
+  )
+}
+
+export default RelatedCard;
