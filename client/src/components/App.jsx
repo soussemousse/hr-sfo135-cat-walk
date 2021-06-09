44ab119ea2b3dfd17aee4bcc5eded20bb3a2fdf1
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-06-08 19:24:14 -0700

    Added Related Products component to main App.jsx file.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductOverview from '../modules/ProductOverview/ProductOverview.jsx';
+import Related from '../modules/RelatedProducts/RelatedProductsYourOutfit.jsx';
 
 
 const App = function () {
   return (
     <div>
       <ProductOverview />
+      <Related />
     </div>
   );
 }
