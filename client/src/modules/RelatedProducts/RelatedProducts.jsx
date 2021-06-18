57b4536dec4341d0f7b8ac9b7b6f93d29f238bbf
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-06-18 11:01:23 -0700

    Refactoring from shep code review.

--- client/src/modules/RelatedProducts/RelatedProducts.jsx
+++ client/src/modules/RelatedProducts/RelatedProducts.jsx
@@ -13,20 +13,45 @@ class RelatedProducts extends React.Component {
     }
   }
 
-  openCompare = (product) => {this.setState({compare: true, compareProduct: product})};
+  openCompare = (productOnCarad) => {this.setState({compare: true, compareProduct: product})};
   closeCompare = (event) => {this.setState({compare: false})};
 
   render () {
     return (
       <div className={style.carosel} data-testid='list'>
-        {this.props.relatedInfo.relatedStart !== 0 ? <button className={btnStyle.nav} data-testid='leftClick' onClick={(event) => {this.props.caroselClickLeft('related')}}>&larr;</button> : null}
+        {this.props.relatedInfo.relatedStart !== 0
+        ? <button
+            className={btnStyle.nav}
+            data-testid='leftClick'
+            onClick={(event) => {this.props.caroselClickLeft('related')}}
+          >&larr;</button>
+        : null}
         <div className={style.list}>
           {this.props.products.map((product, index) => {
-            return <ProductCard key={index + 'related' + product.productDetails[0]} product={product} actionButton={this.openCompare} cardClick={this.props.cardClick} list={this.props.list}/>
+            return <ProductCard
+                     key={index + 'related' + product.productDetails[0]}
+                     product={product}
+                     actionButton={this.openCompare}
+                     cardClick={this.props.cardClick}
+                     list={this.props.list}
+                    />
           })}
         </div>
-        {!this.state.compare ? null : <CompareProducts close={this.closeCompare} currentProduct={this.props.relatedInfo.currentProduct} compareProduct={this.state.compareProduct} data-testid='compareProducts'/>}
-        {this.props.relatedInfo.relatedEnd < this.props.relatedInfo.relatedList.length ? <button className={btnStyle.nav} data-testid='rightClick' onClick={(event) => {this.props.caroselClickRight('related')}}>&rarr;</button> : null}
+        {!this.state.compare
+          ? null
+          : <CompareProducts
+              close={this.closeCompare}
+              currentProduct={this.props.relatedInfo.currentProduct}
+              compareProduct={this.state.compareProduct}
+              data-testid='compareProducts'
+            />}
+        {this.props.relatedInfo.relatedEnd < this.props.relatedInfo.relatedList.length
+          ? <button
+              className={btnStyle.nav}
+              data-testid='rightClick'
+              onClick={(event) => {this.props.caroselClickRight('related')}}
+            >&rarr;</button>
+          : null}
       </div>
     )
   }
